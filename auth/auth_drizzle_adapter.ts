9c8dc96c9f7b91eddc0b3503fd3c37edff9abd10
Brian Takita <brian.takita@gmail.com>
2024-02-15 18:01:22 -0500

    ./schema:
    	user→user_tbl
    	account→account_tbl
    	session→session_tbl
    	verification_token→verification_token_tbl
    
    update dependencies

--- auth/auth_drizzle_adapter.ts
+++ auth/auth_drizzle_adapter.ts
@@ -2,7 +2,7 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { drizzle_db_ } from '@rappstack/domain--server/drizzle'
 import { and, eq } from 'drizzle-orm'
 import { type wide_app_ctx_T } from 'relysjs/server'
-import { account, user } from '../schema/index.js'
+import { account_tbl, user_tbl } from '../schema/index.js'
 export function auth_drizzle_adapter_(app_ctx:wide_app_ctx_T) {
 	return {
 		...DrizzleAdapter(drizzle_db_(app_ctx)),
@@ -13,12 +13,12 @@ export function auth_drizzle_adapter_(app_ctx:wide_app_ctx_T) {
 			const results =
 				drizzle_db_(app_ctx)
 					.select()
-					.from(account)
-					.leftJoin(user, eq(user.user_id, account.user_id))
+					.from(account_tbl)
+					.leftJoin(user_tbl, eq(user_tbl.user_id, account_tbl.user_id))
 					.where(
 						and(
-							eq(account.provider, provider_account.provider),
-							eq(account.provider_account_id, provider_account.providerAccountId)))
+							eq(account_tbl.provider, provider_account.provider),
+							eq(account_tbl.provider_account_id, provider_account.providerAccountId)))
 					.get()
 			return results?.user ?? null
 		},
--- schema/index.ts
+++ schema/index.ts
@@ -1,23 +1,23 @@
 import type { AdapterAccount } from '@auth/core/adapters'
 import { integer, primaryKey, sqliteTable, text, } from 'drizzle-orm/sqlite-core'
-export const user = sqliteTable('user', {
+export const user_tbl = sqliteTable('user', {
 	user_id: text('user_id').notNull().primaryKey(),
 	name: text('name'),
 	email: text('email'),
-	email_verified: integer('email_verified', { mode: 'timestamp_ms' }),
+	email_verify_dts: integer('email_verify_dts', { mode: 'timestamp_ms' }),
 	image: text('image'),
 })
-export const account = sqliteTable('account', {
+export const account_tbl = sqliteTable('account', {
 	user_id:
 		text('user_id')
 			.notNull()
-			.references(()=>user.user_id, { onDelete: 'cascade' }),
+			.references(()=>user_tbl.user_id, { onDelete: 'cascade' }),
 	type: text('type').$type<AdapterAccount['type']>().notNull(),
 	provider: text('provider').notNull(),
 	provider_account_id: text('provider_account_id').notNull(),
 	refresh_token: text('refresh_token'),
 	access_token: text('access_token'),
-	expires_at: integer('expires_at'),
+	expire_dts: integer('expire_dts', { mode: 'timestamp_ms' }),
 	token_type: text('token_type'),
 	scope: text('scope'),
 	id_token: text('id_token'),
@@ -27,18 +27,18 @@ export const account = sqliteTable('account', {
 		columns: [table.provider, table.provider_account_id]
 	})
 }))
-export const session = sqliteTable('session', {
+export const session_tbl = sqliteTable('session', {
 	session_token: text('session_token').notNull().primaryKey(),
 	user_id:
 		text('user_id')
 			.notNull()
-			.references(()=>user.user_id, { onDelete: 'cascade' }),
-	expires: integer('expires', { mode: 'timestamp_ms' }).notNull(),
+			.references(()=>user_tbl.user_id, { onDelete: 'cascade' }),
+	expire_dts: integer('expire_dts', { mode: 'timestamp_ms' }).notNull(),
 })
-export const verification_token = sqliteTable('verification_token', {
+export const verification_token_tbl = sqliteTable('verification_token', {
 	identifier: text('identifier').notNull(),
 	token: text('token').notNull(),
-	expires: integer('expires', { mode: 'timestamp_ms' }).notNull(),
+	expire_dts: integer('expire_dts', { mode: 'timestamp_ms' }).notNull(),
 }, table=>({
 	pk: primaryKey({
 		columns: [table.identifier, table.token]
