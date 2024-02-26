689360fa833512bc7b2bb6f7eb7d179ed348e7cc
Brian Takita <brian.takita@gmail.com>
2024-02-26 01:15:56 -0500

    ./auth:
    	persontric: ns: app
    	auth_adapter: ns: app
    	+ auth_adapter__set

--- auth/persontric.ts
+++ auth/persontric.ts
@@ -1,24 +1,25 @@
 import { DrizzleSQLiteAdapter } from '@persontric/adapter-drizzle'
 import { drizzle_db_ } from '@rappstack/domain--server/drizzle'
+import { ns_id_be_sig_triple_ } from 'ctx-core/rmemo'
 import { Persontric } from 'persontric'
-import { id_be_memo_pair_, id_be_sig_triple_, type request_ctx_T } from 'relysjs/server'
 import { person_tbl, session_tbl } from '../schema/index.js'
 export const [
 	,
 	persontric_,
 	persontric__set,
-] = id_be_sig_triple_(
+] = ns_id_be_sig_triple_(
+	'app',
 	'persontric',
-	(ctx:request_ctx_T)=>
-		new Persontric(
-			auth_adapter_(ctx)))
+	ctx=>new Persontric(
+		auth_adapter_(ctx)))
 export const [
 	,
-	auth_adapter_
-] = id_be_memo_pair_(
+	auth_adapter_,
+	auth_adapter__set,
+] = ns_id_be_sig_triple_(
+	'app',
 	'auth_adapter',
-	(ctx:request_ctx_T)=>new DrizzleSQLiteAdapter(
+	ctx=>new DrizzleSQLiteAdapter(
 		drizzle_db_(ctx),
 		session_tbl,
-		person_tbl)
-)
+		person_tbl))
